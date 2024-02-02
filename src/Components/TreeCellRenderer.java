3b7a72677c662febc492f57c6b8800d086aa2951
Lewis Clark <me@lewisclark.dev>
2024-02-02 12:09:26 +0000

    Change 'sel' to 'selected'

--- src/Components/TreeCellRenderer.java
+++ src/Components/TreeCellRenderer.java
@@ -15,7 +15,7 @@ public class TreeCellRenderer extends DefaultTreeCellRenderer{
 
 	@Override
 	public Component getTreeCellRendererComponent(JTree tree, Object value,
-			boolean sel, boolean expanded, boolean leaf, int row,
+			boolean selected, boolean expanded, boolean leaf, int row,
 			boolean hasFocus) {
 
 		Component c = super.getTreeCellRendererComponent(tree, value,
